3f70b04f2a3badbdb6458e318275b7d509cb5378
TheSoma300 <froghyar.soma@gmal.com>
2021-09-29 14:21:02 +0100

    scrolling sections and modal first steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { forkJoin } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import { finalize, first } from 'rxjs/operators';
 import { CardFactory } from './core/card';
 import { CardIcon, ICard } from './core/interfaces/card.interface';
-import { Booking, Cinema, Movie } from './core/interfaces/cinema.interface';
-import { createItemCard, dashboardNavigationCards } from './core/nav-cards';
+import { FormComponent } from './form/form.component';
 import { CinemaService } from './services/cinema.service';
 
 @Component({
@@ -12,9 +11,18 @@ import { CinemaService } from './services/cinema.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('cinema') cinemaEl!: ElementRef;
+  @ViewChild('movie') movieEl!: ElementRef;
+  @ViewChild('booking') bookingEl!: ElementRef;
+
+  @ViewChild('cinemaNav') cinemaNavEl!: ElementRef;
+  @ViewChild('movieNav') movieNavEl!: ElementRef;
+  @ViewChild('bookingNav') bookingNavEl!: ElementRef;
+
+  bsModalRef?: BsModalRef;
+
   title = 'ultraplex';
-  navCards: ICard[] = dashboardNavigationCards;
   cinemaCards: ICard[] = [];
   movieCards: ICard[] = [];
   bookingCards: ICard[] = [];
@@ -23,18 +31,38 @@ export class AppComponent implements OnInit {
 
   constructor(
     private readonly service: CinemaService,
-    // private readonly modal: BsModalService  
+    private readonly modal: BsModalService  
   ) {}
   ngOnInit(): void {
     console.log('lmao');
     this.getAllData();
   }
-
-
-  selectNavigation(card: ICard): void {
-    this.navCards.forEach(c => c.selected = false)
-    card.selected = true;
+  ngAfterViewInit(): void {
+    this.cinemaEl?.nativeElement.focus();
+    this.movieEl?.nativeElement.focus();
+    this.bookingEl?.nativeElement.focus();
+    this.initNavbarObservers();
   }
+
+
+  selectNavigation($element: HTMLElement): void {
+    $element.scrollIntoView(true);
+  }
+  selectCinema(card: ICard): void {
+    const initialState: ModalOptions = {
+      initialState: {
+        list: [
+          'Open a modal with component',
+          'Pass your data',
+          'Do something else',
+          '...'
+        ],
+        title: 'Modal with component'
+      }
+    };
+    this.bsModalRef = this.modal.show(FormComponent, initialState);
+  }
+  selectMovie(card: ICard): void {}
   openCreateModal(itemType: CardIcon): void {
     console.log(itemType);
     
@@ -53,4 +81,41 @@ export class AppComponent implements OnInit {
       });
 
   }
+  private initNavbarObservers(): void {
+    // const targets = document.getElementsByClassName('section-page')
+
+    const options = {
+      root: null, 
+      rootMargin: '0px',
+      threshold: 0.5
+    }
+    let observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+          document.querySelector('.active')?.classList.remove('active');
+          const sectionId = entry.target.classList[0]; // identify which element is visible in the viewport at 50%
+          console.log(sectionId);
+          switch (sectionId) {
+            case 'cinema-section':
+              this.cinemaNavEl.nativeElement.classList.add('active');
+              break;
+            case 'movie-section':
+              this.movieNavEl.nativeElement.classList.add('active');
+              break;
+            case 'booking-section':
+              this.bookingNavEl.nativeElement.classList.add('active');
+              break;
+            default:
+              break;
+          }
+          // classList.add('active');
+          
+        }
+      });
+    }, options);
+    [this.cinemaEl, this.movieEl, this.bookingEl].forEach(target => observer.observe(target.nativeElement));
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,27 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { SectionComponent } from './section/section.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    SectionComponent
+    SectionComponent,
+    FormComponent
   ],
   imports: [
     CommonModule,
     BrowserModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
